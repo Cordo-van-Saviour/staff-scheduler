f625a3f49cc4baf6411d8855750f6616dc7bd737
ookie <radovanovic.uros92@gmail.com>
2022-08-02 20:05:21 +0200

    Setup for docker

--- seeders/20220802132739-User.js
+++ seeders/20220802132739-User.js
@@ -10,7 +10,8 @@ module.exports = {
       company: 'Microsoft',
       password: '$2a$10$S7UWYkuwdHLD24Onc51g/O8cBEllw.0mIBd98lRFJu8RtZqnzGKNO',
       type: 'staff',
-      createdAt: '2022-08-02T05:54:06.683Z'
+      createdAt: new Date(),
+      updatedAt: new Date()
     },
     {
       id: 'aef33417-da85-4742-bc1e-fc70d168ef75',
@@ -20,8 +21,8 @@ module.exports = {
       company: 'Microsoft',
       password: '$2a$10$IYwNi8JP1WVMI8t5j41no.636wzWgWt1MYMo.VV0IugDKTB5CPw.6',
       type: 'admin',
-      createdAt: '2022-08-02T13:06:33.832Z',
-      updatedAt: '2022-08-02T13:06:33.832Z'
+      createdAt: new Date(),
+      updatedAt: new Date()
     }
     ], {})
   },
