3868c141df11d7a2b4ef9dbf0c48fc04a6d49630
ookie <radovanovic.uros92@gmail.com>
2022-08-02 07:50:36 +0200

    Setup for Heroku deployment pt2

--- config/config.js
+++ config/config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  development: {
+    use_env_variable: 'DB_URI_DEVELOPMENT',
+    dialect: process.env.DB_DIALECT
+  },
+  test: {
+    use_env_variable: 'DB_URI_TEST',
+    dialect: process.env.DB_DIALECT
+  },
   production: {
     use_env_variable: 'DB_URI',
     dialect: 'postgres',
